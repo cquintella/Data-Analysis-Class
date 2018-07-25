4c998ff0b1646623aaa1ed7633de9e368a4d3a56
Carlos Alvaro de M. S. Quintella <caquintella@gmail.com>
2018-07-25 16:06:02 -0300

    Update RedBull.R

--- RedBull.R
+++ RedBull.R
@@ -1,1 +1,9 @@
-ggplot(Amostragem, aes(x=Medida, color=Sabor))+geom_density()
+
+#LOAD
+amostra<-read.csv("redbul.csv", sep=";")
+cnames(amostra)<-c("Sabor","lata", "volume")
+laranjas<-amostra[amostra$Sabor=="uva",]
+uvas<-amostra[amostra$Sabor=="laranja",]
+boxplot(
+
+ggplot(amostra, aes(x=Medida, color=Sabor))+geom_density()
