142090722aba420971ac8934f5396b26fb93210a
Carlos Alvaro de M. S. Quintella <caquintella@gmail.com>
2018-07-25 16:41:34 -0300

    Update RedBull.R

--- RedBull.R
+++ RedBull.R
@@ -1,9 +1,15 @@
 
 #LOAD
 amostra<-read.csv("redbul.csv", sep=";")
-cnames(amostra)<-c("Sabor","lata", "volume")
-laranjas<-amostra[amostra$Sabor=="uva",]
-uvas<-amostra[amostra$Sabor=="laranja",]
-boxplot(
+cnames(amostra)<-c("sabor","lata", "medida")
+head(amostra)
+amostra %>% 
+    group_by(sabor) %>% 
+    summarise(mean(medida, na.rm=TRUE))
+laranjas<-amostra[amostra$sabor=="uva",]
+uvas<-amostra[amostra$sabor=="laranja",]
+uvas<-uvas[!is.na(uvas$medida),]
+laranjas<-laranjas[!is.na(laranjas$medida),]
+boxplot(uvas$medida,laranjas$medida)
 
 ggplot(amostra, aes(x=Medida, color=Sabor))+geom_density()
